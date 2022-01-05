b62daa3d9f32bbc7f1f37c7b214ea0e4de5ac5e6
Aryan61905 <96496036+Aryan61905@users.noreply.github.com>
2022-01-04 22:38:56 -0500

    Adding features for tiles

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const canvas = document.getElementById('game');
 const context = canvas.getContext('2d')
 
 let speed =7;
+
+let tileCount=20;
+let tileSize = canvas.width/ tileCount -2;
+let headX =10;
+let headY=10;
+
+
 function drawGame(){
     clearScreen();
     setTimeout(drawGame, 1000/speed);
