965156ad0195c0d8d9c5840f4530a6b8540a4168
Aryan61905 <96496036+Aryan61905@users.noreply.github.com>
2022-01-04 23:12:37 -0500

    Creating actionKeylisterners for arrow keys

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ let tileSize = canvas.width/ tileCount -2;
 let headX =10;
 let headY=10;
 
+let VelX = 0;
+let VelY = 0;
+
+
 
 function drawGame(){
     clearScreen();
+    changeSnakeposition();
     drawSnake();
     setTimeout(drawGame, 1000/speed);
 
@@ -21,10 +26,49 @@ function clearScreen(){
     context.fillRect(0,0,canvas.clientWidth,canvas.height);
 }
 
+
+
 function drawSnake(){
-    context.fillStyle = '#f76300'
-    context.fillRect(headX*tileCount,headY*tileCount, tileSize,tileSize)
+    context.fillStyle = '#f76300';
+    context.fillRect(headX*tileCount,headY*tileCount, tileSize,tileSize);
     
+}
+
+
+function changeSnakeposition(){
+    headX = headX + VelX;
+    headY = headY + VelY;
+}
+
+
+document.body.addEventListener('keydown',keyDown);
+
+function keyDown(event){
+    if(event.keyCode == 38){//up
+        VelY = -1;
+        VelX = 0;
+
+    }
+    if(event.keyCode == 40){//down
+        VelY = 1;
+        VelX = 0;
+
+    }
+    if(event.keyCode == 37){//left
+        VelY = 0;
+        VelX = -1;
+
+    }
+    if(event.keyCode == 39){//right
+        VelY = 0;
+        VelX = 1;
+
+    }
+
+
+
+
+
 }
 
 drawGame();
