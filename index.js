2c4cd3ff361d89b76ef39851c106eb71803f721a
Aryan61905 <96496036+Aryan61905@users.noreply.github.com>
2022-01-05 21:54:21 -0500

    Adding the body for snake

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const canvas = document.getElementById('game');
 const context = canvas.getContext('2d')
 
+class SnakePart{
+    constructor(x,y){
+        this.x =x;
+        this.y =y;
+    }
+}
+
 let speed =7;
 
 let tileCount=20;
 let tileSize = canvas.width/ tileCount -2;
 let headX =10;
 let headY=10;
+const snakeParts=[];
+let tailLength=2;
+
+
 let appleX=5;
 let appleY=5;
 
@@ -37,12 +48,24 @@ function clearScreen(){
 function drawSnake(){
     context.fillStyle = '#f76300';
     context.fillRect(headX*tileCount,headY*tileCount, tileSize,tileSize);
+    context.fillStyle = "green";
+    for(let i =0; i<snakeParts.length;i++){
+        let part = snakeParts[i];
+        context.fillRect(part.x*tileCount,part.y*tileCount,tileSize,tileSize)
+    }
+    snakeParts.push(new SnakePart(headX,headY));
+    context.fillStyle = '#f76300';
+    context.fillRect(headX*tileCount,headY*tileCount, tileSize,tileSize);
+
+
+
+
+
+    if(snakeParts.length>tailLength){
+        snakeParts.shift();
+    }
     
 }
-
-
-
-
 function changeSnakeposition(){
     headX = headX + VelX;
     headY = headY + VelY;
@@ -58,6 +81,7 @@ function checkApplecollision(){
     if (appleX == headX && appleY == headY){
         appleX = Math.floor(Math.random()*tileCount);
         appleY = Math.floor(Math.random()*tileCount);
+        tailLength++
     }
 }
 
